4509b821e38e03a36ece5c87b6a6daf3140f6c08
sachinkhedekar <47321756+sachinkhedekar@users.noreply.github.com>
2021-03-29 21:17:25 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+Class Advance
+ {
+  public static void main (string args[])
+   {
+     int a =10 ;
+     int b = 20 , c;
+     c = a+b;
+     System.out.println("The addition of this program is" + c );
+     
+    }
+  }
+  
